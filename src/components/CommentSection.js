63b701e7a51362d514ae226e103b973eca80c564
pavelnaradovy <pavel.narodovy@viaduct.pro>
2024-09-27 04:52:06 +0300

    Add styles

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { init, removeById } from "../store/redusers/commentSlice";
+import styles from "./comments.module.sass";
 
 const Comments = () => {
   const comments = useSelector((state) => state.commentSlice.comments);
 
   const dispatch = useDispatch();
+
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch("https://dummyjson.com/comments", {
@@ -23,11 +26,30 @@ const Comments = () => {
   return (
     <div>
       <h1>Comments</h1>
-      <cdiv>
+      <div className={styles.comments}>
         {comments?.map((e) => {
-          return <div>{e.id}</div>;
+          return (
+            <div key={e.id} className={styles.comment}>
+              <img
+                alt="123"
+                src={`https://ui-avatars.com/api/name=${e.body.slice(
+                  0,
+                  1
+                )}&background=random`}
+              />
+              <div className={styles.comment__message}>{e.body}</div>
+            </div>
+          );
         })}
-      </cdiv>
+      </div>
+      <div className={styles.line}>
+        <input
+          placeholder="Text your message"
+          value={message}
+          onChange={(e) => setMessage(e.value)}
+        />
+        <button>Send</button>
+      </div>
     </div>
   );
 };
