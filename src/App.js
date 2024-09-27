ec67cd02c868162c007b056996ddfddf725628da
pavelnaradovy <pavel.narodovy@viaduct.pro>
2024-09-27 03:33:05 +0300

    Refactore

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { init, removeById } from "./store/redusers/commentSlice";
+import Comments from "./components/CommentSection";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/comments", {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((response) => dispatch(init(response.comments)))
-      .then((response) => dispatch(removeById(1)));
-      
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Comments />
     </div>
   );
 }
